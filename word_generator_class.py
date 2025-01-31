72c8f8f118d6cd955b1e2ca8c7feca8031c32203
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-31 19:13:04 +0100

    added exploratory mode

--- word_generator_class.py
+++ word_generator_class.py
@@ -4,20 +4,25 @@ from dictio_creator import create_dictio
 
 
 class WordGenerator:
-	def __init__(self,language:str,vision=2):
+	def __init__(self,language:str,vision=2,exploration=False):
 
 		if not os.path.exists("dictionnaries/"+language+"-n"+str(vision)+".txt"):
 			print(f"Creating a dictionnary for {language} at vision {vision}")
 			create_dictio(language,vision)
 		
 		self.matrix : dict = {}
+		self.exploration = exploration
 
 		with open("dictionnaries/"+language+"-n"+str(vision)+".txt","r") as dictio_file:
 			self.alphabet = "".join(dictio_file.readline().removesuffix("\n").split("\t"))
+			self.general_table = np.zeros(len(self.alphabet))
 			for line in dictio_file.readlines():
 				line_content = line.removesuffix("\n").split("\t")
 				
-				self.matrix[line_content[0]] = [int(number) for number in line_content[1:]]
+				table = np.array([int(number) for number in line_content[1:]])
+				self.general_table = self.general_table + table
+				self.matrix[line_content[0]] = table
+
 
 		self.vision = len(list(self.matrix.keys())[0])
 	
@@ -37,7 +42,12 @@ class WordGenerator:
 	
 
 	def next_letter(self, root:str) -> str:
-		table = np.array(self.matrix[root])
+		if not root in self.matrix.keys():
+			table = self.general_table
+		else:
+			table = np.array(self.matrix[root])
+		if self.exploration :
+			table = table + [1]*len(table)
 		table = table / np.sum(table)
 		letter = np.random.choice(list(self.alphabet),p=table)
 		return letter
